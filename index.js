1ef6be49016dd37c702922c0396ad4e85552db35
Kavindi Perera <ksathperera@gmail.com>
2023-01-25 18:55:03 +0530

    primitives

--- index.js
+++ index.js
@@ -8,3 +8,19 @@
   //constants In constants when we use let keyword we cannot change the value of a variable.Since it is a constant
   const interestRate = 0.3;
   console.log(interestRate);
+
+  //primitives
+  let name2 = 'Kavindi';
+  let age = 23;
+  let isApproved = false;
+  let firstName = undefined;
+  let selectedColor = null;
+
+  console.log(name2);
+  console.log(age);
+  console.log(isApproved);
+  console.log(firstName);
+  console.log(selectedColor);
+  
+
+
